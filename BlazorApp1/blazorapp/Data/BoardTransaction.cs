b5f694a8d3debc5ae6a5a4f20557cf424389bec1
01Prasara <78464166+01Prasara@users.noreply.github.com>
2021-02-07 22:02:23 +0530

    Feb 7th Test run - Bool change

--- BlazorApp1/blazorapp/Data/BoardTransaction.cs
+++ BlazorApp1/blazorapp/Data/BoardTransaction.cs
@@ -8,15 +8,15 @@ namespace BlazorApp.Data
         public string BoardName {get;set;}
         public string Zone1 { get; set; }
 
-        public string Zone2 { get; set; }
+        public bool Zone2 { get; set; }
+
+        public string Zone3 { get; set; }
+
+        public string Zone4 { get; set; }
 
         public DateTime SourceCreatedDateTime { get; set; }
 
         public DateTime CreatedDateTime { get; set; }
-
-        public bool Zone1Status{get;set;}
-
-        public bool Zone2Status {get;set;}
       }
 
     }
